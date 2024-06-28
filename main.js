f2fe3a79a0f28de420fa3bd9bbc796838b628bde
Peter <bachowskip@gmail.com>
2024-06-27 22:55:51 -0700

    Add remove, length, clear, keys, values, entries functions as well as update set function to double the hash map limit and rehash all previous data

--- main.js
+++ main.js
@@ -22,14 +22,15 @@ class HashMap {
 
     set(key, value) {
 
-        // if(this.capacity >= this.bucketLimit*this.loadFactor) { //if the bucket is at the load limit (75%) resize
-        //     this.bucketLimit *= 2;
-        //     let newBucketList = new Array(this.bucketLimit);
-        //     for (let i = 0; i <= this.buckets.length; i++) {
-        //         newBucketList[i] = this.buckets[i];
-        //     }
-        //     this.buckets = newBucketList;
-        // }
+        if (this.capacity >= this.bucketLimit*this.loadFactor) {
+            this.bucketLimit *= 2;
+            let newBuckets = new Array (this.bucketLimit);
+            const entries = this.entries();
+            this.buckets = newBuckets;
+            entries.forEach(element => {
+                this.set(element[0], element[1]);
+            });
+        }
 
         let hashCode = this.hash(key); //hashCode is also the index for the buckets
         let newData = new Node(hashCode, key, value);
@@ -37,6 +38,8 @@ class HashMap {
         if (hashCode < 0 || hashCode >= this.buckets.length) { //places bounds on the buckets
             throw new Error("Trying to access index out of bound");
         }
+
+
 
         if (!this.buckets[hashCode]) {
             this.buckets[hashCode] = newData;
@@ -61,18 +64,17 @@ class HashMap {
             throw new Error("Trying to access index out of bound");
         }
 
-        for (let i = 0; i <= this.buckets.length; i++) {
-            let node = this.buckets[i];
-            if (node && node.hashCode === hashCode) {
-                let curr = node;
-                while (curr !== null) {
-                    if (curr.key === key) {
-                        return curr.value;
-                    }
-                    curr = curr.next;
+        let head = this.buckets[hashCode];
+
+        if (head) {
+            let curr = head;
+            while (curr !== null) {
+                if (curr.key === key) {
+                    return curr.value;
                 }
-                return null;
-            }            
+                curr = curr.next;
+            }
+            return null;
         }
     }
 
@@ -81,17 +83,15 @@ class HashMap {
         if (hashCode < 0 || hashCode >= this.buckets.length) { //places bounds on the buckets
             throw new Error("Trying to access index out of bound");
         }
-        for (let i = 0; i <= this.buckets.length; i++) {
-            let node = this.buckets[i];
-            if (node && node.hashCode === hashCode) {
-                let curr = node;
-                while (curr !== null) {
-                    if (curr.key === key) {
-                        return true;
-                    }
-                    curr = curr.next;
+        let head = this.buckets[hashCode];
+        if (head) {
+            let curr = head;
+            while (curr !== null) {
+                if (curr.key === key) {
+                    return true;
                 }
-            }            
+                curr = curr.next;
+            }
         }
         return false;
     }
@@ -101,27 +101,95 @@ class HashMap {
         if (hashCode < 0 || hashCode >= this.buckets.length) { //places bounds on the buckets
             throw new Error("Trying to access index out of bound");
         }
-        for (let i = 0; i <= this.buckets.length; i++) {
-            let node = this.buckets[i];
-            if (node && node.hashCode === hashCode) {
-                let curr = node;
-                if (curr.key === key) {
-                    if (curr === node && curr.next === null){ //if first node in the bucket
-                        this.buckets[i] = curr.next;
+        let head = this.buckets[hashCode];
+
+        if (head) {
+            let curr = head;
+            while (curr !== null) {
+                if (key === head.key) { //if curr is the first node (head)
+                    if (curr.next === null) {
+                        this.buckets[hashCode] = null;
+                        this.capacity -= 1;
+                        return true;
                     }
                     else {
-                        while (curr !== null) {
-                            if ()
-                            curr = curr.next;
-                        }
-                        
+                        this.buckets[hashCode] = head.next;
+                        this.capacity -= 1;
+                        return true;
                     }
-                    return true;
+                }
+                else if (curr.next.key === key) {
+                    if (curr.next.next !== null) {
+                        curr.next = curr.next.next;
+                        this.capacity -= 1;
+                        return true;
+                    }
+                    else {
+                        curr.next = null;
+                        this.capacity -= 1;
+                        return true;
+                    }
                 }
                 curr = curr.next;
-            }            
+            }
         }
         return false;
+    }
+
+    length () {
+        return this.capacity;
+    }
+
+    clear () {
+        this.bucketLimit = 16;
+        this.capacity = 0;
+        this.buckets = new Array (this.bucketLimit);
+    }
+
+    keys () {
+        let array = [];
+        for (let i = 0; i <= this.buckets.length; i++) {
+            let head = this.buckets[i];
+            if (head) {
+                let curr = head;
+                while (curr !== null) {
+                    array.push(curr.key);
+                    curr = curr.next;
+                }
+            }
+        }
+        return array;
+    }
+
+    values () {
+        let array = [];
+        for (let i = 0; i <= this.buckets.length; i++) {
+            let head = this.buckets[i];
+            if (head) {
+                let curr = head;
+                while (curr !== null) {
+                    array.push(curr.value);
+                    curr = curr.next;
+                }
+            }
+        }
+        return array;
+    }
+
+    entries () {
+        let array = [];
+        for (let i = 0; i <= this.buckets.length; i++) {
+            let head = this.buckets[i];
+            if (head) {
+                let curr = head;
+                while (curr !== null) {
+                    const entry = [curr.key, curr.value];
+                    array.push(entry);
+                    curr = curr.next;
+                }
+            }
+        }
+        return array;
     }
 }
 
@@ -147,10 +215,6 @@ test.set('ice cream', 'white');
 test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
-//test.set('moon', 'silver');
+test.set('moon', 'silver');
 
 console.log(test.buckets);
-console.log(test.bucketLimit);
-console.log(test.capacity);
-console.log(test.get('hat'));
-console.log(test.has('lion'));
